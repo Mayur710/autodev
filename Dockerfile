a264ef6d02341e8699fd8f183ce3a189870f6448
Mayur710 <mayurkr.verma@gmail.com>
2026-08-19 18:17:08 +0530

    CSV cleaning task - fixing windows UTF-8 ecoding error , tested multi-step data cleaning

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,7 @@ FROM python:3.11-slim
 
 WORKDIR /app
 
-RUN pip install --no-cache-dir pytest
-
-# CMD ["python", "code.py"]
+COPY requirements.txt .
+RUN pip install --no-cache-dir -r requirements.txt
 
 CMD ["pytest", "-v"]
--- nodes/executor.py
+++ nodes/executor.py
@@ -9,38 +9,45 @@ import uuid
 SOLUTION_FILENAME = "solution.py"
 TESTS_FILENAME = "test_solution.py"
 DOCKER_IMAGE_NAME = "autodev-sandbox"
+DATA_FILENAME = "sample_data.csv"
 timeout_secs = 15
 
 def executor(state):
     #save code to file 
-    with open(SOLUTION_FILENAME, "w") as f:
+    with open(SOLUTION_FILENAME, "w", encoding="utf-8") as f:
         f.write(state["code"])
 
-    with open(TESTS_FILENAME, "w") as f:
+    with open(TESTS_FILENAME, "w", encoding="utf-8") as f:
         f.write(state["tests"])
 
     #building full path to mount on docker 
     solution_host_path = os.path.abspath(SOLUTION_FILENAME)
     tests_host_path = os.path.abspath(TESTS_FILENAME)
+    data_host_path = os.path.abspath(DATA_FILENAME)
     container_name = f"autodev-run-{uuid.uuid4().hex[:8]}"  # Generate a unique container name
+    docker_args = [
+        "docker",
+        "run",
+        "--rm",
+        "--name", container_name,
+        "--network", "none",
+        "--memory", "512m",
+        "--cpus", "1",
+        "-v", f"{solution_host_path}:/app/solution.py",
+        "-v", f"{tests_host_path}:/app/test_solution.py",
+    ]
+    if os.path.exists(data_host_path):
+        docker_args += ["-v", f"{data_host_path}:/app/{DATA_FILENAME}"]
+
+    docker_args.append(DOCKER_IMAGE_NAME)
     try:
-        result = subprocess.run(
-            [
-                "docker",
-                "run",
-                "--rm",
-                "--name", container_name,
-                "--network", "none",
-                "--memory", "512m",
-                "--cpus", "1",
-                "-v", f"{solution_host_path}:/app/solution.py",
-                "-v", f"{tests_host_path}:/app/test_solution.py",
-                DOCKER_IMAGE_NAME,
-            ],
-            capture_output=True,
-            text=True,
-            timeout=timeout_secs
+        result = subprocess.run(           
+                docker_args,
+                capture_output=True,
+                text=True,
+                timeout=timeout_secs
         )
+
         if(result.returncode == 0):
             state["success"] = True
             state["output"] = result.stdout
